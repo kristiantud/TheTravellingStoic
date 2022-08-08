bcda5bbd0048b03c8492e948ebd81e975db28fbf
Kristian Tud <kristiantud@hotmail.com>
2022-08-07 23:16:05 -0600

    added mobile version of beerolympics22

---
+++ express.js
@@ -0,0 +1,15 @@
+// var express = require("express");
+// var app = express();
+
+// app.get("/", function(req, res){
+// 	res.sendFile(__dirname + '/\index.html');	
+
+// });
+
+
+// app.get("/calgary", function(req, res){
+// 	res.sendFile(__dirname + '/\calgary.html');	
+
+// });
+
+// app.listen(3000, () => console.log("listening on port 3000"));
